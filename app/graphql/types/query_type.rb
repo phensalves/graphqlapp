ae8c828ef9f8c5183da49d4fed248f586c1c8f26
Pedro Alves <pedro_alves@me.com>
2019-03-06 11:16:51 -0300

    removing required argument

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -12,7 +12,7 @@ Types::QueryType = GraphQL::ObjectType.define do
   # end
   field :user, Types::UserType do
     argument :id, types.ID
-    argument :name, !types.String
+    argument :name, types.String
     description "User identify"
 
     resolve ->(obj, args, ctx) {
